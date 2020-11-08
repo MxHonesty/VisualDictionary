822963b48693f94c44c1523294e03a2f9ac9f059
Stelian <stoianstelian33@gmail.com>
2020-11-08 22:20:11 +0200

    first ocr tests

--- app/main.py
+++ app/main.py
@@ -3,15 +3,3 @@ import pytesseract as pt
 
 
 pt.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
-img = cv2.imread("test_images\\test1.png")
-img2 = cv2.imread("test_images\\test2.jpg")
-img3 = cv2.imread("test_images\\test3.jpg")
-
-text = pt.image_to_string(img)
-print(text)
-
-text = pt.image_to_string(img2)
-print(text)
-
-text = pt.image_to_string(img3)
-print(text)
--- app/tests/ocr_test.py
+++ app/tests/ocr_test.py
@@ -2,3 +2,45 @@
     anumite imagini preselectate. """
     
 import unittest
+import cv2
+import pytesseract as pt
+import os 
+
+def strip_output(text):
+    text = " ".join(text.split())
+    text = text.replace("\n", " ")
+    return text
+
+
+pt.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+
+class TestOCR(unittest.TestCase):
+    """ Clasa de test pentru recunoasterea unor imagini selectate. """
+    def test_image1(self):
+        img = cv2.imread("test_images\\test1.png")
+        text = pt.image_to_string(img)
+        self.assertEqual(strip_output(text), "((( BREAKING NEWS )))")
+    
+    def test_image2(self):
+        img = cv2.imread("test_images\\test2.jpg")
+        text = pt.image_to_string(img)
+        self.assertEqual(strip_output(text), "Link-uri text si imagine in HTML")
+            
+    def test_image3(self):
+        img = cv2.imread("test_images\\test3.jpg")
+        text = pt.image_to_string(img)
+        self.assertEqual(strip_output(text), "NU STIU CE M-AS FACE FARA TINE")
+    
+    def test_image4(self):
+        img = cv2.imread("test_images\\test4.png")
+        text = pt.image_to_string(img)
+        self.assertEqual(strip_output(text), "selectonist s. m. (sil. -0-), pl. selectionisti sursa: Ortografic (2002) adaugata de siveco _actiuni =")
+        
+    def test_image6(self):
+        img = cv2.imread("test_images\\test6.png")
+        text = pt.image_to_string(img)
+        self.assertEqual(strip_output(text), "A testcase is created by subclassing unittest. testcase. The three individual tests are defined with methods whose names start with the letters test. This naming convention informs the test runner about which methods represent tests.")
+        
+
+if __name__ == "__main__":
+    unittest.main()
