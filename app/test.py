6c1da89a41124e5824339c8e9d959f894a774837
Stelian <stoianstelian33@gmail.com>
2020-11-10 10:39:31 +0200

    teste centralizate

---
+++ app/test.py
@@ -0,0 +1,7 @@
+""" Modulul responsabil pentru rularea tuturor testelor. """
+from domain.localizertest import LocalizerTest
+from tests.ocr_test import TestOCR
+import unittest
+
+if __name__ == "__main__":
+    unittest.main()
--- app/tests/ocr_test.py
+++ app/tests/ocr_test.py
@@ -11,39 +11,33 @@ def strip_output(text):
     text = " ".join(text.split())
     text = text.replace("\n", " ")
     return text
-
-
+    
 pt.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
 class TestOCR(unittest.TestCase):
     """ Clasa de test pentru recunoasterea unor imagini selectate. """
     def test_image1(self):
-        img = cv2.imread("test_images\\test1.png")
+        img = cv2.imread("..\\app\\tests\\test_images\\test1.png")
         text = pt.image_to_string(img)
         self.assertEqual(strip_output(text), "((( BREAKING NEWS )))")
     
     def test_image2(self):
-        img = cv2.imread("test_images\\test2.jpg")
+        img = cv2.imread("..\\app\\tests\\test_images\\test2.jpg")
         text = pt.image_to_string(img)
         self.assertEqual(strip_output(text), "Link-uri text si imagine in HTML")
             
     def test_image3(self):
-        img = cv2.imread("test_images\\test3.jpg")
+        img = cv2.imread("..\\app\\tests\\test_images\\test3.jpg")
         text = pt.image_to_string(img)
         self.assertEqual(strip_output(text), "NU STIU CE M-AS FACE FARA TINE")
     
     def test_image4(self):
-        img = cv2.imread("test_images\\test4.png")
+        img = cv2.imread("..\\app\\tests\\test_images\\test4.png")
         text = pt.image_to_string(img)
         self.assertEqual(strip_output(text), "selectonist s. m. (sil. -0-), pl. selectionisti sursa: Ortografic (2002) adaugata de siveco _actiuni =")
-        
-    def test_image6(self):
-        img = cv2.imread("test_images\\test6.png")
-        text = pt.image_to_string(img)
-        self.assertEqual(strip_output(text), "A testcase is created by subclassing unittest. testcase. The three individual tests are defined with methods whose names start with the letters test. This naming convention informs the test runner about which methods represent tests.")
-        
+    
     def test_imagine7(self):
-        img = cv2.imread("test_images\\test7.jpg")
+        img = cv2.imread("..\\app\\tests\\test_images\\test7.jpg")
         text = pt.image_to_string(img)
         print(strip_output(text))
 
